001f5533ee41d41333d3fbb8f31d1b05d0ed5cbb
Alvin <>
2020-12-09 13:16:54 +0400

    injecting the logging service into components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from './../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input()
@@ -12,10 +14,13 @@ export class AccountComponent {
   id!: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService: LoggingService) {
+
+  }
 
   // tslint:disable-next-line:typedef
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,12 +1,18 @@
+import { LoggingService } from './../logging.service';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService) {
+
+  }
 
   // tslint:disable-next-line:typedef
   onCreateAccount(accountName: string, accountStatus: string) {
@@ -14,6 +20,6 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
